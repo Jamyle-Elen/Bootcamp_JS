79cc4e95dceef0ab182c1421258cce2efc7948bf
Jamyle Elen Cardoso do Nascimento <110051309+Jamyle-Elen@users.noreply.github.com>
2024-01-07 18:24:00 -0300

    Exercicio - Calculo do valor a ser pago

---
+++ 02-condicionais/valorSerPago.js
@@ -0,0 +1,10 @@
+/*
+    Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço normal de etiqueta e a escolha da condição de pagamento.
+    Utilize os codigos de tabela a seguir para ler qual a condição de pagamento escolhida e efetuar o calculo adequado.
+
+    Código Condição de pagamento.:
+    - À vista Débito, recebe 10% de desconto;
+    - À vista no Dinheiro ou PIX, recene 15% de desconto;
+    - Em duas vezes, preço normal de etiqueta sem juros;
+    - Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
+*/
