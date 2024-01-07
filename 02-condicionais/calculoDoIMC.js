2443756a36713e51ff9117e2b8db466f4cb45563
Jamyle Elen Cardoso do Nascimento <110051309+Jamyle-Elen@users.noreply.github.com>
2024-01-07 08:22:06 -0300

    Calculo do IMC

---
+++ 02-condicionais/calculoDoIMC.js
@@ -0,0 +1,11 @@
+// Formula do IMC.:
+// IMC = Peso / (altura + altura)
+
+// Elabore um algoritmo que dado o peso e altura de um adulto mostre sua condição de acordo com a tabela abaixo.:
+
+// IMC em adultos condição.:
+// - Abaixo de 18.5, abaixo do peso;
+// - Entre 18.5 e 25, peso normal;
+// - Entre 25 e 50, acima do peso;
+// - Entre 30 e 40, obeso;
+// - Acima de 40, obesidade grave;
