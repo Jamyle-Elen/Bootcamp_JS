ecaaeb3de8982b0da94876a249e0c0831fdd7e91
Jamyle Elen Cardoso do Nascimento <110051309+Jamyle-Elen@users.noreply.github.com>
2024-01-07 08:32:54 -0300

    Resolução

--- 02-condicionais/calculoDoIMC.js
+++ 02-condicionais/calculoDoIMC.js
@@ -9,3 +9,22 @@
 // - Entre 25 e 50, acima do peso;
 // - Entre 30 e 40, obeso;
 // - Acima de 40, obesidade grave;
+
+const peso = 54.6
+const altura = 1.68;
+const IMC = peso / (altura * altura);
+const IMCFormat = IMC.toFixed(1)
+
+if (IMCFormat < 18.5) {
+    console.log('Abaixo do peso')
+} else if (IMCFormat >= 18.5 && IMCFormat <= 25) {
+    console.log('Peso normal')
+} else if (IMCFormat >= 25 && IMCFormat <= 50) {
+    console.log('Acima do peso')
+} else if (IMCFormat >= 30 && IMCFormat <= 40) {
+    console.log('Obeso')
+} else {
+    console.log('Obesidade grave')
+}
+
+console.log(IMCFormat)
