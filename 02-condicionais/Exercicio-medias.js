1702cf56df0372d3f3cf7c3275b3243bf639cf81
Jamyle Elen Cardoso do Nascimento <110051309+Jamyle-Elen@users.noreply.github.com>
2024-01-07 07:49:40 -0300

    Exercicio de media

---
+++ 02-condicionais/Exercicio-medias.js
@@ -0,0 +1,8 @@
+// Faça um algoritmo que dado três notas tiradas por um aluno em um semestre da faculdade cal]cule e imprima sua média e sua classificação conforme a tabela abaixo.:
+
+// Média = (n1 + n2 + n3) \ 3
+
+// Classificação.;
+// Menor que 5, reprovado;
+// Média entre 5 e 7, recuperação;
+// Maior que 7, passou de semestre;
