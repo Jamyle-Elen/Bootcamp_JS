98276f20f8affa3b967d74ec749a1abe91178c9d
Jamyle Elen Cardoso do Nascimento <110051309+Jamyle-Elen@users.noreply.github.com>
2024-01-16 19:13:43 -0300

    Update imc-com-funcoes.js

--- 04-funcoes/imc-com-funcoes.js
+++ 04-funcoes/imc-com-funcoes.js
@@ -1,16 +1,3 @@
-/*
-    Formula do IMC.:
-    IMC = Peso / (altura + altura)
-
-    Elabore um algoritmo que dado o peso e altura de um adulto mostre sua condição de acordo com a tabela abaixo.:
-
-    IMC em adultos condição.:
-    - Abaixo de 18.5, abaixo do peso;
-    - Entre 18.5 e 25, peso normal;
-    - Entre 25 e 50, acima do peso;
-    - Entre 30 e 40, obeso;
-    - Acima de 40, obesidade grave;
-*/
 
 function calcularIMC(peso, altura) {
   return peso / Math.pow(altura * altura);
