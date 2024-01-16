fb3370e671c8dd9ceaccbdeeca72d9621a9351fa
Jamyle Elen Cardoso do Nascimento <110051309+Jamyle-Elen@users.noreply.github.com>
2024-01-16 20:43:28 -0300

    Update aula_funcao2.js

--- 04-funcoes/aula_funcao2.js
+++ 04-funcoes/aula_funcao2.js
@@ -1,6 +1,17 @@
 function escrevaMeuNome(nome) {
-    console.log(`Meu nome é ${nome}`);
+  console.log(`Meu nome é ${nome}`);
 }
 
-escrevaMeuNome('Jamyle');
-escrevaMeuNome('Rhuan');
+escrevaMeuNome("Jamyle");
+escrevaMeuNome("Rhuan");
+
+function verificarIdade(idade) {
+  if (idade >= 18) {
+    console.log(escrevaMeuNome('Jamyle')``);
+  } else {
+    console.log("Menor");
+  }
+}
+
+verificarIdade(13);
+verificarIdade(25);
