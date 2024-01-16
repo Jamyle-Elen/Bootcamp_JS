986ce7278552a4ebf04cda58cae7ddaf0179897e
Jamyle Elen Cardoso do Nascimento <110051309+Jamyle-Elen@users.noreply.github.com>
2024-01-16 18:35:12 -0300

    Update aula.js

--- 04-funcoes/aula.js
+++ 04-funcoes/aula.js
@@ -1,6 +1,8 @@
-function quadrado(valor) {
-    return valor * valor;
+function incrementarJuros(valor, percentualJuros) {
+    const valorDeAcrescimo = (percentualJuros / 100) * valor;
+    return (percentualJuros / 100)
 }
 
-const quadradoDeDez = quadrado(10);
-console.log(quadradoDeDez)
+console.log(incrementarJuros(100,10));
+console.log(incrementarJuros(100,15));
+console.log(incrementarJuros(100,20));
