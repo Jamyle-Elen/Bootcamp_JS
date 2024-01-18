c0cfb9af3857445f442ac4e309941233e0d537ed
Jamyle Elen Cardoso do Nascimento <110051309+Jamyle-Elen@users.noreply.github.com>
2024-01-18 01:56:43 -0300

    Update valorSerPago_funcoes.js

--- 04-funcoes/valorSerPago_funcoes.js
+++ 04-funcoes/valorSerPago_funcoes.js
@@ -2,18 +2,20 @@ const valorRealProduto = 100;
 const formaDePagamento = 4;
 
 
-if (formaDePagamento === 1) {
-  console.log("Selecionado.: À Vista Débito");
-  console.log(valorRealProduto - (valorRealProduto * 0.1));
-} else if (formaDePagamento === 2) {
-  console.log("Selecionado.: À Vista Dinheiro ou PIX");
-  console.log(valorRealProduto - (valorRealProduto * 0.15));
-} else if (formaDePagamento === 3) {
-  console.log("Selecionado.: Duas vezes");
-  console.log(valorRealProduto);
-} else if (formaDePagamento === 4) {
-  console.log("Selecionado.: +2x");
-  console.log(valorRealProduto + (valorRealProduto * 0.1));
-} else {
-  console.log("Forma de pagamento inválida!");
+function aplicarDesconto(valor, desconto) {
+  if (formaDePagamento === 1) {
+    console.log("Selecionado.: À Vista Débito");
+    console.log(valorRealProduto - valorRealProduto * 0.1);
+  } else if (formaDePagamento === 2) {
+    console.log("Selecionado.: À Vista Dinheiro ou PIX");
+    console.log(valorRealProduto - valorRealProduto * 0.15);
+  } else if (formaDePagamento === 3) {
+    console.log("Selecionado.: Duas vezes");
+    console.log(valorRealProduto);
+  } else if (formaDePagamento === 4) {
+    console.log("Selecionado.: +2x");
+    console.log(valorRealProduto + valorRealProduto * 0.1);
+  } else {
+    console.log("Forma de pagamento inválida!");
+  }
 }
