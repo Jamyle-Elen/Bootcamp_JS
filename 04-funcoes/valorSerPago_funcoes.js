69fe4d9572513f7124c4e23098029b8a4f145d8d
Jamyle <110051309+Jamyle-Elen@users.noreply.github.com>
2024-02-01 20:10:50 -0300

    valor a ser pago - funções

--- 04-funcoes/valorSerPago_funcoes.js
+++ 04-funcoes/valorSerPago_funcoes.js
@@ -3,21 +3,25 @@ const formaDePagamento = 4;
 
 
 function aplicarDesconto(valor, desconto) {
-  return(valor - (valorRealProduto * 0.1))
+  return(valor - (valor * (desconto / 100)));
 }
 
-// if (formaDePagamento === 1) {
-//   console.log("Selecionado.: À Vista Débito");
-//   console.log(valorRealProduto - valorRealProduto * 0.1);
-// } else if (formaDePagamento === 2) {
-//   console.log("Selecionado.: À Vista Dinheiro ou PIX");
-//   console.log(valorRealProduto - valorRealProduto * 0.15);
-// } else if (formaDePagamento === 3) {
-//   console.log("Selecionado.: Duas vezes");
-//   console.log(valorRealProduto);
-// } else if (formaDePagamento === 4) {
-//   console.log("Selecionado.: +2x");
-//   console.log(valorRealProduto + valorRealProduto * 0.1);
-// } else {
-//   console.log("Forma de pagamento inválida!");
-// }
+function acrescimoJuros(valor, juros) {
+  return(valor + (valor * (juros / 100)));
+}
+
+if (formaDePagamento === 1) {
+  console.log("Selecionado.: À Vista Débito");
+  console.log(aplicarDesconto(valorRealProduto, 10));
+} else if (formaDePagamento === 2) {
+  console.log("Selecionado.: À Vista Dinheiro ou PIX");
+  console.log(aplicarDesconto(valorRealProduto, 15));
+} else if (formaDePagamento === 3) {
+  console.log("Selecionado.: Duas vezes");
+  console.log(valorRealProduto);
+} else if (formaDePagamento === 4) {
+  console.log("Selecionado.: +2x");
+  console.log(acrescimoJuros(valorRealProduto, 10));
+} else {
+  console.log("Forma de pagamento inválida!");
+}
