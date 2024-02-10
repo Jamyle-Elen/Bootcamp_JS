caa0806a6fcaf76bd150d1d1379f22efd08d8e57
Jamyle <110051309+Jamyle-Elen@users.noreply.github.com>
2024-02-10 14:57:11 -0300

    comparar pessoas

--- 05-objetos/script.js
+++ 05-objetos/script.js
@@ -1,19 +1,30 @@
 class Pessoa {
     nome;
     idade;
+    anoDeNascimento;
+
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2023 - idade;
+    }
 
     descrever() {
         console.log(`Minha idade é ${this.nome} e minha idade é ${this.idade}`);
     }
 }
 
-const jamyle = new Pessoa();
-jamyle.nome = 'Jamyle Elen C do Nascimento';
-jamyle.idade = 18;
+function compararPessoas(p1, p2) {
+    if (p1.idade > p2.idade) {
+        console.log(`${p1.nome} é mais velha que ${p2.nome}.`);
+    } else if (p1.idade < p2.idade) {
+        console.log(`${p2.nome} é mais velho que ${p1.nome}.`);
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} são da mesma idade.`);
+    }
+}
 
-const rhuan = new Pessoa();
-rhuan.nome = 'Rhuan Pablo M da Silva';
-rhuan.idade = 18;
+const jamyle = new Pessoa('Jamyle Elen', 18);
+const rhuan = new Pessoa('Rhuan Pablo', 18);
 
-jamyle.descrever();
-rhuan.descrever();
+compararPessoas(jamyle, rhuan);
