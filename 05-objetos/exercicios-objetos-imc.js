5902a86b66e5f645492e1dcb928ab474c5a8ebf3
Jamyle <110051309+Jamyle-Elen@users.noreply.github.com>
2024-02-11 15:24:49 -0300

    atividade - classe para representar carros

---
+++ 05-objetos/exercicios-objetos-imc.js
@@ -0,0 +1,8 @@
+/*
+
+        2) Crie uma classe para representar pessoas.
+        Para cada pessoa teremos os atributos nome, peso e altura.
+        As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC =  peso / (altura * altura))
+
+
+*/
