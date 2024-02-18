9f5c9430a4f99a8527ce7c9418d78b597c120a2f
Jamyle <110051309+Jamyle-Elen@users.noreply.github.com>
2024-02-18 06:54:04 -0300

    atividade

--- 05-objetos/exercicios-objetos-imc.js
+++ 05-objetos/exercicios-objetos-imc.js
@@ -17,13 +17,27 @@ class Pessoa {
     }
 
     calcularIMC() {
-        return this.peso / Math.pow(this.altura, 2);
+        return this.peso / (this.altura * this.altura);
+    }
+    classificarIMC() {
+        const imc = this.calcularIMC().toFixed(2);
+        if (imc < 18.5) {
+            return 'Abaixo do peso';
+        } else if (imc >= 18.5 && imc < 25) {
+            return 'Peso normal';
+        } else if (imc >= 25 && imc < 30) {
+            return 'Acima do peso';
+        } else if (imc >= 30 && imc < 35) {
+            return 'Obeso';
+        } else {
+            return 'Obesidade grave';
+        }
     }
 }
 
-const jose = new Pessoa('José', 70, 1.75);
-console.log(jose.calcularIMC().toFixed(2));
-const rhuan = new Pessoa('Rhuan', 70, 1.80);
-console.log(rhuan.calcularIMC().toFixed(2));
+const jose = new Pessoa('José', 79, 1.75);
+console.log(jose.classificarIMC());
+const rhuan = new Pessoa('Rhuan', 65, 1.80);
+console.log(rhuan.classificarIMC());
 const jamyle = new Pessoa('Jamyle', 54, 1.68);
-console.log(jamyle.calcularIMC().toFixed(2));
+console.log(jamyle.classificarIMC());
